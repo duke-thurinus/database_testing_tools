696a6f5d30854c5197d90ab8bfe7457dfffd3b9d
Henry <hbalch@bai.org>
2024-09-05 14:11:39 -0500

    debug mode

--- db_tester.py
+++ db_tester.py
@@ -2,36 +2,57 @@ import pyodbc
 import pandas as pd
 import configparser
 import test_creator
+import argparse
 
+def RunTests(tests, conn):
+    for test in tests:
+        df1 = pd.read_sql(test["SQL_Query1"], conn)
+        columns = df1.columns.to_list()
+        df1.sort_values(by=columns, inplace=True, ignore_index=True)
+
+        df2 = pd.read_sql(test["SQL_Query2"], conn)
+        df2.sort_values(by=columns, inplace=True, ignore_index=True)
+
+        comparisonDF = df1.compare(df2)
+        if comparisonDF.empty:
+            print('test with value: ' + str(test["val"]) + ' is successful')
+        else:
+            print('test with value: ' + str(test["val"]) + ' produced diffrences:')
+            print(comparisonDF)
+
+def PrintTests(tests):
+    i = 0
+    for test in tests:
+        print("Test " + str(i))
+        print(test["SQL_Query1"])
+        print(test["SQL_Query2"])
+        print("---------")
+        i += 1
+
+## Command Line Arguments Parse
+commandParser = argparse.ArgumentParser()
+commandParser.add_argument("--debug", help="prints sql commands instead of running them",
+                    action="store_true")
+args = commandParser.parse_args()
+## End Command Line Arguments Parse
+
+## Config File Parse
 configP = configparser.ConfigParser()
 configP.read('config.ini')
-
-SERVER = configP['Database Connection Values']['SERVER']
-DATABASE = configP['Database Connection Values']['DATABASE']
-USERNAME = configP['Database Connection Values']['USERNAME']
-PASSWORD = configP['Database Connection Values']['PASSWORD']
-
-connectionString = f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
-
-conn = pyodbc.connect(connectionString)
+## End Config File Parse
 
 tests = test_creator.loadTestsFromJsonFile(configP['Database Connection Values']['TESTFOLDERPATH'])
 
-for test in tests:
-    df1 = pd.read_sql(test["SQL_Query1"], conn)
-    columns = df1.columns.to_list()
-    df1.sort_values(by=columns, inplace=True, ignore_index=True)
+if args.debug:
+    PrintTests(tests)
+else:
+    SERVER = configP['Database Connection Values']['SERVER']
+    DATABASE = configP['Database Connection Values']['DATABASE']
+    USERNAME = configP['Database Connection Values']['USERNAME']
+    PASSWORD = configP['Database Connection Values']['PASSWORD']
 
-    df2 = pd.read_sql(test["SQL_Query2"], conn)
-    df2.sort_values(by=columns, inplace=True, ignore_index=True)
+    connectionString = f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
 
-    comparisonDF = df1.compare(df2)
-    if comparisonDF.empty:
-        print('test with value: ' + str(test["val"]) + ' is successful')
-    else:
-        print('test with value: ' + str(test["val"]) + ' produced diffrences:')
-        print(comparisonDF)
-
-conn.close()
-
-
+    conn = pyodbc.connect(connectionString)
+    RunTests(tests, conn)
+    conn.close()
